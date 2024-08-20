d10dd96f65c0ea20f610bf8a975f716ea0b53bf6
Sudipta Pandit <sudpandit@microsoft.com>
2024-08-20 17:08:56 +0530

    fix: gitignore

---
+++ cmd/gbench/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"flag"
+	"time"
+
+	"github.com/realsdx/ttrpc-bench/worker"
+)
+
+func main() {
+	// parse flags for concurrenty and time of run
+	// flags
+	concurrency := flag.Int("c", 1, "Number of concurrent requests")
+	duration := flag.Int("t", 5, "How long to run this test. In seconds")
+	flag.Parse()
+
+	host := flag.Arg(0)
+
+	runner := worker.GRPCRunner{
+		ServerAddress: host,
+		Concurrency:   *concurrency,
+		Duration:      time.Duration(*duration),
+	}
+	runner.Run()
+}
